cf54a6aaa0085b675a5942a9dd1dce17f71642e8
Daniel martínez <dmartinezh97@gmail.com>
2023-05-04 01:10:58 +0200

    Volver al spawn

--- v2/Example/transcribir.py
+++ v2/Example/transcribir.py
@@ -5,6 +5,5 @@ import base64
 audio = sys.argv[1]
 model = whisper.load_model("medium") #medium
 result = model.transcribe(audio, fp16=False)
-print(result["text"])
-# texto_en_base64 = base64.b64encode(bytes(result["text"], 'utf-8')).decode("utf-8")
-# print(texto_en_base64)
+texto_en_base64 = base64.b64encode(bytes(result["text"], 'utf-8')).decode("utf-8")
+print(texto_en_base64)
